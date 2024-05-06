182c0db22968560d73b02fbfd45862c035942ed7
“heyapurv” <deskofapurv@gmail.com>
2024-05-06 17:28:23 +0530

    padding

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -23,7 +23,7 @@ export default function TableComponent() {
                                     <tr key={idx}>
                                         <td className="sm:pr-6 py-4 whitespace-nowrap">{item.Name}</td>
                                         <td className="pr-6 py-4 whitespace-nowrap">
-                                            <span className={`sm:px-3 px-1 py-2 rounded-full font-semibold text-xs ${item.Completion == "Yes" ? "text-green-600 bg-green-50" : "text-red-600 bg-red-200"}`}>
+                                            <span className={`sm:px-3 px-2 py-2 rounded-full font-semibold text-xs ${item.Completion == "Yes" ? "text-green-600 bg-green-50" : "text-red-600 bg-red-200"}`}>
                                                 {item.Completion == "Yes"? "Completed": "Incomplete"}
                                             </span>
                                         </td>
