9e68593b07704836508f0ca7640b62af128504d2
“heyapurv” <deskofapurv@gmail.com>
2024-05-13 21:32:14 +0530

    AI

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -32,7 +32,7 @@ export default function TableComponent() {
                                         `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
                                     )}
                                     >
-                                    GEN Ai Graduates  <NumberTicker value={totalCompletions  } className="ml-2 font-bold italic mx-1"/> 
+                                    GEN AI Graduates  <NumberTicker value={totalCompletions  } className="ml-2 font-bold italic mx-1"/> 
                                     </span>
                                     <span className="relative">😉</span>
 
